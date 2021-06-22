e5c8da323bbd0b6b67835f53bae4f2f34a35efe7
Trillium <spiteless@gmail.com>
2021-06-22 10:57:38 -0500

    Updating alpha site

--- src/features/heroFeature.jsx
+++ src/features/heroFeature.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { Container } from '@material-ui/core';
+
+import HeroComponent from '../components/heroComponent';
 
 export default function HeroFeature() {
-    return (
-        <h1>
-            HeroFeature
-        </h1>
-    )
+  return (
+    <Container style={{margin: 0, padding: 0 }}>
+      <HeroComponent />
+      <h1>Hero Feature</h1>
+    </Container>
+  );
 }
